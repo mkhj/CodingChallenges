efd0b426c23ca7ea9d08d0d33acb6ca245ca52a6
Mark Jensen <donnib@gmail.com>
2016-06-18 13:02:40 +0200

    First question

---
+++ CodingInterviewQuestions/csharp/csharp.tests/Chapter1Tester.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace csharp.tests
+{
+	[TestFixture()]
+	public class Chapter1Tester
+	{
+		[Test()]
+		public void Run_Q1_1()
+		{
+			Assert.IsTrue(Q1_1.IsUnique("abcde"));
+
+			Assert.IsFalse(Q1_1.IsUnique("hello"));
+
+			Assert.IsFalse(Q1_1.IsUnique("helLo"));
+
+		}
+	}
+}
+
