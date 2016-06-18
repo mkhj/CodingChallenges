1f6f4feba1f3a33f2a48b0e52c208644063d4084
Mark Jensen <donnib@gmail.com>
2016-06-18 15:06:10 +0200

    init

---
+++ MicrosoftCodeChallenge/MSCodeChallenge/MSCodeChallenge.Tests/Test.cs
@@ -0,0 +1,105 @@
+﻿using NUnit.Framework;
+using System;
+namespace MSCodeChallenge.Tests
+{
+	[TestFixture()]
+	public class Test
+	{
+		[Test()]
+		public void Factorial()
+		{
+			var challenges = new Questions();
+
+			Assert.IsTrue(challenges.Factorial(3) == 6);
+
+			Assert.IsTrue(challenges.Factorial(5) == 120);
+
+			Assert.IsTrue(challenges.Factorial(7) == 5040);
+		}
+
+		[Test()]
+		public void FindAverage()
+		{
+			var challenges = new Questions();
+
+			var numbers = new int[] { 1, 2, 3, 4, 5 };
+
+			Assert.IsTrue(challenges.FindAverage(numbers) == 3);
+		}
+
+		[Test()]
+		public void FindFrequence()
+		{
+			var challenges = new Questions();
+
+			var text = "hello world";
+			var chr = 'l';
+
+			Assert.IsTrue(challenges.FindFrequence(text, chr) == 3);
+		}
+
+		[Test()]
+		public void Largest()
+		{
+			var challenges = new Questions();
+
+			var a = 20;
+			var b = 10;
+
+			Assert.IsTrue(challenges.Largest(a, b) == 20);
+
+			Assert.IsTrue(challenges.Largest(b, a) == 20);
+		}
+
+		[Test()]
+		public void Palindrom()
+		{
+			var challenges = new Questions();
+
+
+			Assert.IsFalse(challenges.Palindrom("apple"));
+
+			Assert.IsTrue(challenges.Palindrom("abba"));
+
+			Assert.IsTrue(challenges.Palindrom("racecar"));
+		}
+
+		[Test()]
+		public void SecondHighst()
+		{
+			var challenges = new Questions();
+
+
+			var numbers = new int[] { 1, 4, 3, 2, 5 };
+
+			Assert.IsTrue(challenges.SecondHighst(numbers) == 4);
+		}
+
+		[Test()]
+		public void SumEven()
+		{
+			var challenges = new Questions();
+
+			Assert.IsFalse(challenges.SumEven(9) == 30);
+
+			Assert.IsTrue(challenges.SumEven(10) == 30);
+		}
+
+		[Test()]
+		public void SumSquares()
+		{
+			var challenges = new Questions();
+
+			Assert.IsTrue(challenges.SumSquares(5) == 30);
+		}
+
+		[Test()]
+		public void Volume()
+		{
+			var challenges = new Questions();
+
+			Assert.IsTrue(challenges.Volume(3, 3, 3) == 27);
+		}
+	}
+}
+
---
+++ MicrosoftCodeChallenge/MSCodeChallenge/MSCodeChallenge/IQuestions.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace MSCodeChallenge
+{
+	/// <summary>
+	/// A list of some of the potential questions you could be presented with.
+	/// You have 10-15 min so solve as many as possible.
+	/// You are not allow to use any libraries etc.
+	/// </summary>
+	public interface IQuestions
+	{
+		/// <summary>
+		/// Findet Factorial af tallene op til og med number f.eks. Input: 5 Output: 120 
+		/// </summary>
+		/// <param name="number"></param>
+		/// <returns></returns>
+		long Factorial(int number);
+
+		/// <summary>
+		/// Finder summen af alle lige tal op til limit.
+		/// </summary>
+		/// <param name="limit"></param>
+		/// <returns></returns>
+		int SumEven(int limit);
+
+		/// <summary>
+		/// Findet det andet højeste tal i Numbers f.eks. Input: 6,2,34,7,9 Output: 9
+		/// </summary>
+		/// <param name="numbers"></param>
+		/// <returns></returns>
+		int SecondHighst(int[] numbers);
+
+		/// <summary>
+		/// Finder antallet af omtrædener af FrequenceOf i text.
+		/// </summary>
+		/// <param name="text"></param>
+		/// <param name="FrequenceOf"></param>
+		/// <returns></returns>
+		int FindFrequence(String text, char FrequenceOf);
+
+		/// <summary>
+		/// Finder gennemsnitter af tallene i numbers
+		/// </summary>
+		/// <param name="numbers"></param>
+		/// <returns></returns>
+		int FindAverage(int[] numbers);
+
+		/// <summary>
+		/// Find summen af tallene i anden op til number. F.eks. input: 5 Output: 30
+		/// </summary>
+		/// <param name="number"></param>
+		/// <returns></returns>
+		long SumSquares(int number);
+
+		/// <summary>
+		/// Finder ud af om input er et Palimdrom, altså hvis man vender teksten om, om det er det samme som før, altså f.eks. Abba = True, og Ost = False
+		/// </summary>
+		/// <param name="input"></param>
+		/// <returns></returns>
+		bool Palindrom(String input);
+
+		/// <summary>
+		/// Find det største tal, af a og b
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <returns></returns>
+		int Largest(int a, int b);
+
+		/// <summary>
+		/// Finder volumen ud fra højden, breden, og dybten
+		/// </summary>
+		/// <param name="height"></param>
+		/// <param name="width"></param>
+		/// <param name="depth"></param>
+		/// <returns></returns>
+		int Volume(int height, int width, int depth);
+
+		/// <summary>
+		/// Tæl antallet af ord. f.eks. input: "Hello, World!" , output = 2
+		/// </summary>
+		/// <param name="text"></param>
+		/// <returns></returns>
+		int WordCount(String text);
+	}
+}
+
